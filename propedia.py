810661a1a91dc0436084c4454b971bff613985b8
BillM <moustakasbasilis@gmail.com>
2020-04-02 12:40:08 +0300

    Ekana mia allagh me mhnuma sthn arxh kai kapies mikroallages

--- propedia.py
+++ propedia.py
@@ -1,3 +1,5 @@
-x=int(input('Dwse x : '))
+# Programma propaidias me lambda 
+print('Auto einai ena programma propaidias me stoxo thn ekpaideush se auth')
+x=int(input('Tha doume thn propaidia tou arithmou ?: '))
 y=lambda a: [int(i*a) for i in range(1,11)]
 print(y(x))
