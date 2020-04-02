8096b4d311a2fcf6493e150d374f04eeae096477
BillM <moustakasbasilis@gmail.com>
2020-04-02 12:54:16 +0300

    Ebala kai enan xairetismo sto telos

--- propedia.py
+++ propedia.py
@@ -3,3 +3,4 @@ print('Auto einai ena programma propaidias me stoxo thn ekpaideush se auth')
 x=int(input('Tha doume thn propaidia tou arithmou ?: '))
 y=lambda a: [int(i*a) for i in range(1,11)]
 print(y(x))
+print('Sas xairetw kai kleinw to programma!!')
